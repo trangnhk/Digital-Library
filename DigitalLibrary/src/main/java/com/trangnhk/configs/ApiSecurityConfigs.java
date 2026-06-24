a46bb2ee31ff80f2d131f45f3ac002d64a8cbc60
trang ngo <kieutrangg30805@gmail.com>
2026-06-25 02:25:34 +0700

    fix bug

--- DigitalLibrary/src/main/java/com/trangnhk/configs/ApiSecurityConfigs.java
+++ DigitalLibrary/src/main/java/com/trangnhk/configs/ApiSecurityConfigs.java
@@ -97,7 +97,7 @@ public class ApiSecurityConfigs {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:*", "https://*.onrender.com", "https://*.pages.dev"));
+        config.setAllowedOriginPatterns(List.of("http://localhost:*", "https://*.onrender.com", "https://*.pages.dev"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Set-Cookie"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
